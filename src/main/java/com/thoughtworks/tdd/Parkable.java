6a10f06d2ce028f23070192579fcdeeccfab7000
REET CHEN (ITA-ISDC-ISD-OOCLL/ZHA) <CHENRE6@OOCL.COM>
2019-07-15 20:24:34 +0800

    Add Parkable's function so that code can be simple.And change ParkingLot's implement method.

--- src/main/java/com/thoughtworks/tdd/Parkable.java
+++ src/main/java/com/thoughtworks/tdd/Parkable.java
@@ -1,7 +1,13 @@
 package com.thoughtworks.tdd;
 
+import java.util.List;
+
 public interface Parkable {
     ParkTicket park(Car car);
 
     Car fetch(ParkTicket ticket);
+
+    boolean contains(ParkTicket ticket);
+
+    boolean isFull();
 }
--- src/main/java/com/thoughtworks/tdd/ParkingLot.java
+++ src/main/java/com/thoughtworks/tdd/ParkingLot.java
@@ -1,39 +1,45 @@
 package com.thoughtworks.tdd;
 
+import com.thoughtworks.tdd.Exception.NotEnoughPosition;
+
 import java.util.HashMap;
 import java.util.Map;
 
-public class ParkingLot {
+public class ParkingLot implements Parkable{
     private final int capacity;
     private Map<ParkTicket,Car> cars = new HashMap<ParkTicket,Car>();
-    private boolean dispatchStatus;
 
     public ParkingLot() {
         this.capacity = 10;
-        this.dispatchStatus = false;
     }
 
     public ParkingLot(int capacity) {
         this.capacity = capacity;
-        this.dispatchStatus = false;
     }
 
     public ParkTicket park(Car car) {
-        if(capacity > cars.size() && car != null){
-            ParkTicket ticket = new ParkTicket();
-            cars.put(ticket,car);
-            return ticket;
-        }else {
-            return null;
+        if(isFull()){
+            throw new NotEnoughPosition();
         }
+        if(car == null){return null;}
+
+        ParkTicket ticket = new ParkTicket();
+        cars.put(ticket,car);
+        return ticket;
+
     }
 
     public Car fetch(ParkTicket ticket) {
-        if(cars.containsKey(ticket)){
-            return cars.remove(ticket);
-        }else {
-            return null;
-        }
+        return cars.remove(ticket);
+    }
+
+    @Override
+    public boolean contains(ParkTicket ticket) {
+        return cars.containsKey(ticket);
+    }
+
+    public boolean isFull(){
+        return capacity <= cars.size();
     }
 
     public int getCapacity() {
@@ -44,11 +50,5 @@ public class ParkingLot {
         return cars;
     }
 
-    public boolean getDispatchStatus() {
-        return dispatchStatus;
-    }
-
-    public void setDispatchStatus(boolean dispatchStatus) {
-        this.dispatchStatus = dispatchStatus;
-    }
 }
+
